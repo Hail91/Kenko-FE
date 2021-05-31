80c51b0c02a1ae63d99870f71ad9dff5c14a7022
Aaron <aarongillies88@gmail.com>
2021-05-31 12:54:49 -0400

    begin process of integrating Redux into app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/app.js";
 import * as serviceWorker from "./serviceWorker";
+// Application state imports via Redux library
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "../src/store/reducers/rootReducer";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
