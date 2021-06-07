2b42f8c3a892819e71173c5708ed9744ba43f9bf
Aaron <aarongillies88@gmail.com>
2021-06-07 16:28:09 -0400

    built skeleton userReducer and updated rootReducer

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,1 +1,14 @@
-// Reducer to accept and execute state changes for user here....
+// Action imports
+// Set initial state for user
+const initialState = {
+  // define shape of user data
+};
+// Define reducer
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+// Export reducer to the rootReducer
+export default userReducer;
