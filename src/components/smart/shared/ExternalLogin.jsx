50af486b05cb282d56c63de67c3829ff83cbf40a
Aaron <aarongillies88@gmail.com>
2021-06-30 15:05:05 -0400

    modified modal styling to better fit app theme

--- src/components/smart/shared/ExternalLogin.jsx
+++ src/components/smart/shared/ExternalLogin.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ExternalLogin = ({ facebookLogin, fbAuth, handleModalClick }) => {
+const ExternalLogin = ({ handleModalClick }) => {
   return (
     <div className="mt-6 grid grid-cols-2 gap-3">
       <div>
         <button
           onClick={handleModalClick}
-          className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+          className="w-full inline-flex justify-center py-2 px-4 focus:outline-none border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
         >
           <span className="sr-only">Sign in with Facebook</span>
           <svg
