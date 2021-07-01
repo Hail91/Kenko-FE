926d7c91f480a75eea90d0bb23ef8528fd6182c2
Aaron <aarongillies88@gmail.com>
2021-07-01 17:12:28 -0400

    Facebook auth working as expected in the socialAuthModal, returns an access code which is then parsed and sent to the API to be handled

--- src/store/actions/socialAuthActions/fbAuth.js
+++ src/store/actions/socialAuthActions/fbAuth.js
@@ -1,10 +1,29 @@
+// Imports
+import axios from "axios";
+
 // Create action types
 export const FB_AUTH_START = "FB_AUTH_START";
 export const FB_AUTH_SUCCESS = "FB_AUTH_SUCCESS";
 export const FB_AUTH_FAILURE = "FB_AUTH_FAILURE";
 
-const fbAuth = () => async (dispatch) => {
+const fbAuth = (code) => async (dispatch) => {
   dispatch({ type: FB_AUTH_START });
-  dispatch({ type: FB_AUTH_SUCCESS });
+  // Try/catch block which will call the API endpoint that calls the Facebook endpoint to issue an accessToken
+  try {
+    // Make API call
+    let response = await axios.post(
+      `${process.env.REACT_APP_DEV_URL}/api/auth/social/facebook/token`,
+      {
+        code: code,
+      }
+    );
+    /* After making the above call, response should contain user data that we can pass to the dispatch success 
+    in the payload, which will then be persisted to the store/localStorage */
+    dispatch({ type: FB_AUTH_SUCCESS });
+  } catch (error) {
+    // Log error and dispatch failure
+    console.log(error);
+    dispatch({ type: FB_AUTH_FAILURE, payload: error });
+  }
 };
 export default fbAuth;
